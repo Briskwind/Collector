d577060cef1df621c02b14c335ca6786fb984ba0
briskwind <943138563@qq.com>
2018-10-16 19:02:16 +0800

    Update no sql injections

--- server/extensions/sqlite_conn.py
+++ server/extensions/sqlite_conn.py
@@ -2,6 +2,8 @@ import sqlite3
 
 import os
 
+from django.db import connection
+
 from server.settings import BASE_DIR
 
 DB_FILE = os.path.join(BASE_DIR, 'db.sqlite3')
@@ -17,14 +19,20 @@ def sqlite_executor(sql):
     return res
 
 
+
+
 def get_one_user(user_id):
     """ 获取单个用户，测试sql 注入"""
-    conn = sqlite3.connect(DB_FILE)
+
+    # 防止Sql注入的sql执行方式
+    # sql2 = "select * from home_user WHERE id=%s"
+    # with connection.cursor() as cursor:
+    #     cursor.execute(sql2, [user_id])
+    #     res = cursor.fetchall()
 
     sql = 'select * from home_user WHERE id={};'.format(user_id)
-    print('sql', sql)
+    conn = sqlite3.connect(DB_FILE)
     cursor = conn.cursor()
     cursor.execute(sql)
     res = cursor.fetchall()
     return res
-# sql select * from home_user WHERE id=2 AND SUBSTR((SELECT COALESCE(model,' ') FROM django_content_type LIMIT 5,1),7,1)>'l';
