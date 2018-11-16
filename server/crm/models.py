6d271eb8e30b50981fa78c42d944c20018222ef6
briskwind <943138563@qq.com>
2018-11-16 12:56:53 +0800

    Update table

--- server/crm/models.py
+++ server/crm/models.py
@@ -4,16 +4,13 @@ from django.db import models
 
 # Create your models here.
 
-
-
-
 class News(models.Model):
     """ 新闻模型"""
 
     text = models.CharField('标题', max_length=64)
     url = models.CharField('链接', max_length=256)
     news_type = models.CharField('新闻类型', max_length=64)
-    hot_count = models.IntegerField('新闻热度',blank=True, null=True,)
+    hot_count = models.IntegerField('新闻热度', blank=True, null=True)
     images = models.CharField('图片', max_length=512, default='')
     platform = models.URLField('平台', null=True, blank=True, default='')
     creation_time = models.DateTimeField('创建时间', default=datetime.now)
@@ -24,3 +21,43 @@ class News(models.Model):
     class Meta:
         verbose_name = '新闻资讯'
         verbose_name_plural = '新闻资讯'
+
+
+class BookTag(models.Model):
+    """ 图书标签"""
+
+    tag_name = models.CharField('标签', max_length=64)
+    create_time = models.DateTimeField('创建时间', default=datetime.now)
+
+    def __str__(self):
+        return self.tag_name
+
+    class Meta:
+        verbose_name = '图书标签'
+        verbose_name_plural = '图书标签'
+
+
+class Book(models.Model):
+    """ 图书名称"""
+    book_id = models.CharField('豆瓣id', max_length=64)
+    name = models.CharField('书名称', max_length=128)
+    author = models.CharField('作者', max_length=256)
+    price = models.CharField('价格', max_length=128)
+    publish = models.CharField('出版社', max_length=256)
+    score = models.IntegerField('评分', default=0)
+    cover = models.CharField('封面', max_length=256)
+    url = models.CharField('链接', max_length=256)
+    introduction = models.CharField('简介', max_length=512)
+    tag = models.ManyToManyField(BookTag,verbose_name="标签",related_name="books")
+    create_time = models.DateTimeField('创建时间', default=datetime.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '图书'
+        verbose_name_plural = '图书'
+
+
+
+
--- server/z_tem.py
+++ server/z_tem.py
@@ -1,10 +1,102 @@
+import os
+import sys
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "server.settings")
+import django
+
+django.setup()
+
+from crm.models import BookTag, Book
+
 import requests
+from bs4 import BeautifulSoup
 
-url = 'https://book.douban.com/tag/%E9%98%BF%E5%8A%A0%E8%8E%8E%C2%B7%E5%85%8B%E9%87%8C%E6%96%AF%E8%92%82'
+url = 'https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4?start=960&type=T'
 res = requests.get(url)
-print(res.content.decode())
+
+text = res.content
+soup = BeautifulSoup(text, "lxml")
+items = soup.find_all('li', {'class': "subject-item"})
+
+"""
+
+book_id = models.CharField('豆瓣id', max_length=64)
+name = models.CharField('书名称', max_length=128)
+
+"""
+
+for item in items[:2]:
+
+    cover = item.find('a', {'class': "nbg"}).find('img').attrs['src']
+    url = item.find('a', {'class': "nbg"}).attrs['href']
+    introduction = item.find('p').text.replace('\n', '')
+    score = item.find('span', {'class': "rating_nums"}).text
+    data = item.find('div', {'class': "pub"}).text.replace('\n', '').replace(' ', '').split('/')
+    author = data[0]
+    price = data[-1]
+    publish = data[-3]
+
+    name = item.find('h2').text.replace('\n', '').replace(' ', '')
+    tem_data = url.split('/')
+    book_id = tem_data[-2]
+
+    print('name', name)
+    print('book_id', book_id)
+    print('score', score)
+    print('url', url)
+    print('cover', cover)
+    print('introduction', introduction)
+    print('author', author)
+    print('price', price)
+    print('publish', publish)
+    print('+++++++++++++++')
+
+"""
+ <li class="subject-item">
+<div class="pic">
+<a class="nbg" href="https://book.douban.com/subject/25862578/" onclick="moreurl(this,{i:'0',query:'',subject_id:'25862578',from:'book_subject_search'})">
+<img class="" src="https://img3.doubanio.com/view/subject/m/public/s27264181.jpg" width="90"/>
+</a>
+</div>
+<div class="info">
+<h2 class="">
+<a href="https://book.douban.com/subject/25862578/" onclick="moreurl(this,{i:'0',query:'',subject_id:'25862578',from:'book_subject_search'})" title="解忧杂货店">
+
+    解忧杂货店
 
 
 
 
+  </a>
+</h2>
+<div class="pub">
 
+
+  [日] 东野圭吾 / 李盈春 / 南海出版公司 / 2014-5 / 39.50元
+
+      </div>
+<div class="star clearfix">
+<span class="allstar45"></span>
+<span class="rating_nums">8.6</span>
+<span class="pl">
+        (325958人评价)
+    </span>
+</div>
+<p>现代人内心流失的东西，这家杂货店能帮你找回——
+僻静的街道旁有一家杂货店，只要写下烦恼投进卷帘门的投信口，第二天就会在店后的牛奶箱里得到回答。
+因男友身患绝... </p>
+<div class="ft">
+<div class="collect-info">
+</div>
+<div class="cart-actions">
+<span class="market-info">
+<a href="https://book.douban.com/subject/25862578/?channel=subject_list&amp;platform=web" target="_blank">在豆瓣购买</a>
+</span>
+</div>
+</div>
+</div>
+</li>
+
+
+
+"""
